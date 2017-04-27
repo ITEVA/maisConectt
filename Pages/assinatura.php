d7eac69b354f5f713f798039e428994f4fb907a9
Naldo Maranhão <reginaldo.maranhao@iteva.org.br>
2017-04-27 12:00:34 -0300

    Página de Assinaturas Responsiva

--- Pages/assinatura.php
+++ Pages/assinatura.php
@@ -3,18 +3,18 @@
         <div class="intro-text">
             <div class="row-fluid">
                 <div id="bandaLarga" class="animated fadeInDownBig txtBanner">
-                    <img src="Images/image1.png">
+                    <img src="Images/image1.PNG">
                 </div>
             </div>
             <div class="row-fluid">
                 <div id="oferta" class="animated fadeInLeftBig txtBanner margemTop">
-                    <img src="Images/image2.png">
+                    <img src="Images/image2.PNG">
                 </div>
                 <div id="mega20" class="animated fadeInRightBig txtBanner margemTop">
-                    <img src="Images/image3.png">
+                    <img src="Images/image3.PNG">
                 </div>
             </div>
-            <a href="#services" class="page-scroll btn btn-xl animated fadeIn">Detalhes</a>
+            <a href="#services" class="page-scroll btn btn-xl animated fadeIn banner">Detalhes</a>
         </div>
         <div id="conteudo"></div>
 
@@ -29,77 +29,77 @@
     <div class="col-lg-10 box">
         <h1 id="titulo">PLANOS</h1>
         <div class="col-lg-12 plano">
-            <div class="col-lg-2 banda">
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 banda">
                 <img src="Images/10mega.JPG" alt="">
             </div>
 
-            <div class="col-lg-3 preco">
+            <div class="col-lg-3 col-md-3 col-sm-2 col-xs-2 preco">
                 <p><sup>R$</sup>70,00</p>
             </div>
 
-            <div class="col-lg-3 taxa">
-                <p>Download: XMbps</p>
-                <p>Upload: XMbps</p>
+            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 taxa">
+                <i class="fa fa-download" title="Download">&nbsp;<p>XMbps</p></i><p>Download: XMbps</p>
+                <i class="fa fa-upload" title="Upload">&nbsp;<p>XMbps</p></i><p>Upload: XMbps</p>
             </div>
 
-            <div class="col-lg-3 assinar">
+            <div class="col-lg-3 col-md-3 col-sm-2 col-xs-2 assinar">
                 <p><a href="" class="page-scroll btn btn-xl">Assine</a></p>
             </div>
         </div>
 
         <div class="col-lg-12 plano">
-            <div class="col-lg-2 banda">
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 banda">
                 <img src="Images/15mega.JPG" alt="">
             </div>
 
-            <div class="col-lg-3 preco">
+            <div class="col-lg-3 col-md-3 col-sm-2 col-xs-2 preco">
                 <p><sup>R$</sup>80,00</p>
             </div>
 
-            <div class="col-lg-3 taxa">
-                <p>Download: XMbps</p>
-                <p>Upload: XMbps</p>
+            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 taxa">
+                <i class="fa fa-download" title="Download">&nbsp;<p>XMbps</p></i><p>Download: XMbps</p>
+                <i class="fa fa-upload" title="Upload">&nbsp;<p>XMbps</p></i><p>Upload: XMbps</p>
             </div>
 
-            <div class="col-lg-3 assinar">
+            <div class="col-lg-3 col-md-3 col-sm-2 col-xs-2 assinar">
                 <p><a href="" class="page-scroll btn btn-xl">Assine</a></p>
             </div>
         </div>
 
         <div class="col-lg-12 plano destaque">
-            <div class="col-lg-2 banda">
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 banda">
                 <img src="Images/20mega.JPG" alt="">
             </div>
 
-            <div class="col-lg-3 preco">
+            <div class="col-lg-3 col-md-3 col-sm-2 col-xs-2 preco">
                 <p class="ppDestaque"><sup>R$</sup>90,00</p>
             </div>
 
-            <div class="col-lg-3 taxa">
-                <p class="pDestaque">Download: XMbps</p>
-                <p class="pDestaque">Upload: XMbps</p>
+            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 taxa">
+                <i class="fa fa-download" title="Download">&nbsp;<p>XMbps</p></i><p>Download: XMbps</p>
+                <i class="fa fa-upload" title="Upload">&nbsp;<p>XMbps</p></i><p>Upload: XMbps</p>
             </div>
 
-            <div class="col-lg-3 assinar">
+            <div class="col-lg-3 col-md-3 col-sm-2 col-xs-2 assinar">
                 <p><a href="" class="page-scroll btn btn-xl aDestaque">Assine</a></p>
             </div>
         </div>
 
         <div class="col-lg-12 plano">
-            <div class="col-lg-2 banda">
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 banda">
                 <img src="Images/35mega.JPG" alt="">
             </div>
 
-            <div class="col-lg-3 preco">
+            <div class="col-lg-3 col-md-3 col-sm-2 col-xs-2 preco">
                 <p><sup>R$</sup>100,00</p>
             </div>
 
-            <div class="col-lg-3 taxa">
-                <p>Download: XMbps</p>
-                <p>Upload: XMbps</p>
+            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 taxa">
+                <i class="fa fa-download" title="Download">&nbsp;<p>XMbps</p></i><p>Download: XMbps</p>
+                <i class="fa fa-upload" title="Upload">&nbsp;<p>XMbps</p></i><p>Upload: XMbps</p>
             </div>
 
-            <div class="col-lg-3 assinar">
+            <div class="col-lg-3 col-md-3 col-sm-2 col-xs-2 assinar">
                 <p><a href="" class="page-scroll btn btn-xl">Assine</a></p>
             </div>
         </div>
